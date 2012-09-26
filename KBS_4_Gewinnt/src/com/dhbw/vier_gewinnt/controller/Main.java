42e1707a4d93c95725824dd8cc55a79e6c34c4fa
Sebastian <s.boehm91@gmx.de>
2012-09-26 14:36:10 +0200

    Controller Package added

---
+++ KBS_4_Gewinnt/src/com/dhbw/vier_gewinnt/controller/Main.java
@@ -0,0 +1,14 @@
+package com.dhbw.vier_gewinnt.controller;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Test");
+	}
+
+}
